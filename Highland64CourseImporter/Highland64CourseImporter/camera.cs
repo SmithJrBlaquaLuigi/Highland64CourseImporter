62abf1b0003a4d01ea9c58232bf122eede478809
Damon V. Smith Jr <damon0959@gmail.com>
2017-07-09 00:46:36 -0400

    Please help on line 32. thank you.

--- Highland64CourseImporter/Highland64CourseImporter/camera.cs
+++ Highland64CourseImporter/Highland64CourseImporter/camera.cs
@@ -4,22 +4,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
+using System.Windows.Forms;
+using System.IO;
 
 namespace Highland64CourseImporter
 {
     class camera
+ 
     {
         public Vector3 Position = Vector3.Zero;
         public Vector3 Orientation = new Vector3((float)Math.PI, 0f, 0f);
         public float Movespeed = 0.5f;
         public float MouseSensitvity = 0.03f;
+
     }
 }
-public Matrix4 GetViewMatrix()
+static class Matrix4
+{
+    internal static void LookAt(Vector3 position, Vector3 vector3, Vector3 unitY)
+    {
+       
+    }
+}
+//Must have a return type.. Please help on line 32.
+ public static GetViewMatrix()
 {
     Vector3 lookat = new Vector3();
 
-    lookat.X = (float)(Math.Sin((float)Orientation.X) *
-    lookat.Y = (float)
-    lookat.Z = (float)
+    lookat.X = (float)(Math.Sin((float)Orientation.Vertical) * Math.Cos((float)Orientation.Horizontal));
+    lookat.Y = (float)Math.Sin((float)Orientation.Horizontal);
+    lookat.Z = (float)(Math.Cos((float)Orientation.Vertical) * Math.Cos((float)Orientation.Horizontal));
+
+    Vector3 Position = default(Vector3);
+     Matrix4.LookAt(Position, Position + lookat, Vector3.UnitY);
 }
--- highland64courseimporter/highland64courseimporter/form1.cs
+++ highland64courseimporter/highland64courseimporter/form1.cs
@@ -6,10 +6,6 @@ Everyone is permitted to copy and distribute verbatim copies
 of this license document, but changing it is not allowed.
 */
 
-
-
-
-
 using System;
 using System.Collections.Generic;
 using System.Collections;
